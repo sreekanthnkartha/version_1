d007c03dfbe0b44fb096618ad2d4d217ffc0a47a
Ajayjayaraj <ajayjayaraj559@gmail.com>
2023-03-11 11:12:09 +0530

    commit

--- lib/login.dart
+++ lib/login.dart
@@ -19,10 +19,8 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   var _passwordVisible = false;
-  void initState() {  
-
+  void initState() {
     _passwordVisible = false;
-    
   }
 
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
